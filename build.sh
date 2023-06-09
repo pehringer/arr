80ff3f372d6b601285f07d102171fc35ce7d124e
pehringer <pehringer.jacob@gmail.com>
2023-06-08 19:18:26 -0600

    Better scripts.

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+#! /bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "No argument supplied."
+	echo "./build.sh [DATA_STRUCTURE_NAME]"
+	exit 1
+fi
+
 if [ $1 = 'Array' ]; then
 	gcc -shared -lm -I ./include ./src/cds_Array.c -o ./bin/cds_Array.so
 	echo "Check bin directory."
--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
+#! /bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "No argument supplied."
+	echo "./test.sh [DATA_STRUCTURE_NAME]"
+	exit 1
+fi
+
 if [ $1 = 'Array' ]; then
 	gcc -I ./include ./src/cds_Array.c ./test/Test.c ./test/ArrayTests.c -o ./bin/ArrayTests
 	./bin/ArrayTests
