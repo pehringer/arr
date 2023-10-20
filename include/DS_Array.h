23218097a9472b54a357c8c172a8271b55de8444
Jacob Ray Pehringer <jacob.ray.pehringer@gmail.com>
2023-10-19 23:38:38 -0600

    Better location.

--- include/DS_Array.h
+++ include/DS_Array.h
@@ -4,35 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
-Memory Allocation Format
-========================
-
-+-------+ <-- Header is located at the start of the allocation.
-|DS     |
-|Struct |
-|       |
-+-------+ <-- Data is located after header in the allocation (pointer to
-|Index 0|     data is retured by DS_ArrayNew and DS_ArrayResize functions).
-+-------+
-|Index 1|
-+-------+
-|Index 2|
-+-------+
-|Index 3|
-+-------+
-| ...   |
-+-------+
-|Index N|
-+-------+
-*/
-
-struct DS_Array {
-    int (*compare)(const void*, const void*); // Compare two elements.
-    int length;                               // Number of elements in allocation.
-    size_t size;                              // Size of element.
-};
-
 //Allocation And Configuration
 
 void* DS_ArrayNew(size_t size, int length, int (*compare)(const void*, const void*));
--- src/DS_Array.c
+++ src/DS_Array.c
@@ -1,117 +1,147 @@
 #include "DS_Array.h"
 
+/*
+Memory Allocation Format
+========================
+
++---------+ <- Allocation start.
+|Struct   |
+|DS_Array |
+|         |
++---------+ <- Pointer retured by DS_ArrayNew() and DS_ArrayResize() functions.
+|Element 0|
++---------+
+|Element 1|
++---------+
+|Element 2|
++---------+
+|Elemant 3|
++---------+
+| ...     |
++---------+
+|Element N|
++---------+ <- Allocation end.
+
+*/
+
+struct DS_Array {
+    int (*compare)(const void*, const void*); // Compare two elements.
+    int length;                               // Number of elements in allocation.
+    size_t size;                              // Size of element.
+};
+
 void* DS_ArrayNew(size_t size, int length, int (*compare)(const void*, const void*)) {
-    struct DS_Array *ds = malloc(sizeof(struct DS_Array) + length * size);
-    if(ds == 0) {
+    struct DS_Array *a = malloc(sizeof(struct DS_Array) + length * size);
+    if(a == 0) {
         return 0;
     }
-    ds->compare = compare;
-    ds->length = length;
-    ds->size = size;
-    return ds + 1;
+    a->compare = compare;
+    a->length = length;
+    a->size = size;
+    return a + 1;
 }
 
 void DS_ArrayDelete(void* array) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
-    free(ds);
+    struct DS_Array *a = (struct DS_Array*) array - 1;
+    free(a);
 }
 
 int DS_ArrayLength(void* array) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
-    return ds->length;
+    struct DS_Array *a = (struct DS_Array*) array - 1;
+    return a->length;
 }
 
 void* DS_ArrayResize(void *array, int length) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
-    ds = realloc(ds, sizeof(struct DS_Array) + length * ds->size);
-    if(ds == 0) {
+    struct DS_Array *a = (struct DS_Array*) array - 1;
+    a = realloc(a, sizeof(struct DS_Array) + length * a->size);
+    if(a == 0) {
         return 0;
     }
-    ds->length = length;
-    return ds + 1;
+    a->length = length;
+    return a + 1;
 }
 
 void DS_ArrayCopy(void *array, int start, int stop, const void *source) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
-    array = array + start * ds->size;
+    struct DS_Array *a = (struct DS_Array*) array - 1;
+    array = array + start * a->size;
     while(start < stop) {
-        memcpy(array, source, ds->size);
-        array += ds->size;
-        source += ds->size;
+        memcpy(array, source, a->size);
+        array += a->size;
+        source += a->size;
         start++;
     }
 }
 
 void DS_ArrayFill(void *array, int start, int stop, const void *source) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
-    array = array + start * ds->size;
+    struct DS_Array *a = (struct DS_Array*) array - 1;
+    array = array + start * a->size;
     while(start < stop) {
-        memcpy(array, source, ds->size);
-        array += ds->size;
+        memcpy(array, source, a->size);
+        array += a->size;
         start++;
     }
 }
 
 int DS_ArrayMax(void *array, int start, int stop) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
+    struct DS_Array *a = (struct DS_Array*) array - 1;
     void *max = 0;
     int index = -1;
-    array = array + start * ds->size;
+    array = array + start * a->size;
     while(start < stop) {
-        if(max == 0 || ds->compare(array, max) > 0) {
+        if(max == 0 || a->compare(array, max) > 0) {
             max = array;
             index = start;
         }
-        array += ds->size;
+        array += a->size;
         start++;
     }
     return index;
 }
 
 int DS_ArrayMin(void *array, int start, int stop) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
+    struct DS_Array *a = (struct DS_Array*) array - 1;
     void *min = 0;
     int index = -1;
-    array = array + start * ds->size;
+    array = array + start * a->size;
     while(start < stop) {
-        if(min == 0 || ds->compare(array, min) < 0) {
+        if(min == 0 || a->compare(array, min) < 0) {
             min = array;
             index = start;
         }
-        array += ds->size;
+        array += a->size;
         start++;
     }
     return index;
 }
 
 int DS_ArrayCount(void *array, int start, int stop, const void *target) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
+    struct DS_Array *a = (struct DS_Array*) array - 1;
     int count = 0;
-    array = array + start * ds->size;
+    array = array + start * a->size;
     while(start < stop) {
-        if(ds->compare(array, target) == 0) {
+        if(a->compare(array, target) == 0) {
             count++;
         }
-        array += ds->size;
+        array += a->size;
         start++;
     }
     return count;
 }
 
 int DS_ArrayIndex(void *array, int start, int stop, const void *target) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
-    array = array + start * ds->size;
+    struct DS_Array *a = (struct DS_Array*) array - 1;
+    array = array + start * a->size;
     while(start < stop) {
-        if(ds->compare(array, target) == 0) {
+        if(a->compare(array, target) == 0) {
             return start;
         }
-        array += ds->size;
+        array += a->size;
         start++;
     }
     return -1;
 }
 
 void DS_ArraySort(void *array, int start, int stop) {
-    struct DS_Array *ds = (struct DS_Array*) array - 1;
-    qsort(array + start * ds->size, stop - start, ds->size, ds->compare);
+    struct DS_Array *a = (struct DS_Array*) array - 1;
+    qsort(array + start * a->size, stop - start, a->size, a->compare);
 }
