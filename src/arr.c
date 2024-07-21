4ff05777e1fa45d9ad05658b47859fab45ff0740
pehringer <pehringer.jacob@gmail.com>
2024-07-20 21:25:27 -0600

    swapped header order

--- src/arr.c
+++ src/arr.c
@@ -10,8 +10,8 @@ void* arr_Init(const size_t ele, const size_t len) {
     if(hdr == 0) {
         return 0;
     }
-    hdr[0] = len;
-    hdr[1] = ele;
+    hdr[0] = ele;
+    hdr[1] = len;
     char *dat = (char*) hdr + SIZEOF_HDR;
     memset(dat, 0, len * ele);
     return dat;
@@ -24,17 +24,17 @@ void arr_Free(void *arr) {
 
 size_t arr_Len(const void *arr) {
     size_t *hdr = (size_t*) ((char*) arr - SIZEOF_HDR);
-    return hdr[0];
+    return hdr[1];
 }
 
 void* arr_App(void *arr, const void *src, const size_t len) {
     size_t *hdr = (size_t*) ((char*) arr - SIZEOF_HDR);
-    hdr[0] += len;
-    hdr = realloc(hdr, SIZEOF_HDR + hdr[0] * hdr[1]);
+    hdr[1] += len;
+    hdr = realloc(hdr, SIZEOF_HDR + hdr[1] * hdr[0]);
     if(hdr == 0) {
         return 0;
     }
     char *dat = (char*) hdr + SIZEOF_HDR;
-    memcpy(dat + (hdr[0] - len) * hdr[1], src, len * hdr[1]);
+    memcpy(dat + (hdr[1] - len) * hdr[0], src, len * hdr[0]);
     return dat;
 }
