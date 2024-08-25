7a045a7403a7c51675e19edfdc03edae65e1bd60
pehringer <pehringer.jacob@gmail.com>
2024-08-25 12:31:51 -0600

    better names

--- tests/tests.c
+++ tests/tests.c
@@ -27,45 +27,45 @@ void RunTests(int number, struct Test *tests) {
 }
 
 int zero_len_init(void) {
-    int *a = arr_Init(0, 0);
-    if(arr_Len(a) != 0) return 1;
-    arr_Free(a);
-    a = arr_Init(sizeof(int), 0);
-    if(arr_Len(a) != 0) return 1;
-    arr_Free(a);
+    int *a = arr_Create(0, 0);
+    if(arr_Length(a) != 0) return 1;
+    arr_Destroy(a);
+    a = arr_Create(0, sizeof(int));
+    if(arr_Length(a) != 0) return 1;
+    arr_Destroy(a);
     return 0;
 }
 
 int non_zero_len_init(void) {
-    int *a = arr_Init(0, 9);
-    if(arr_Len(a) != 9) return 1;
-    arr_Free(a);
-    a = arr_Init(sizeof(int), 3);
-    if(arr_Len(a) != 3) return 1;
+    int *a = arr_Create(9, 0);
+    if(arr_Length(a) != 9) return 1;
+    arr_Destroy(a);
+    a = arr_Create(3, sizeof(int));
+    if(arr_Length(a) != 3) return 1;
     if(a[0] != 0) return 1;
     if(a[1] != 0) return 1;
     if(a[2] != 0) return 1;
     a[0] = 2;
     a[1] = 3;
     a[2] = 5;
-    if(arr_Len(a) != 3) return 1;
+    if(arr_Length(a) != 3) return 1;
     if(a[0] != 2) return 1;
     if(a[1] != 3) return 1;
     if(a[2] != 5) return 1;
-    arr_Free(a);
+    arr_Destroy(a);
     return 0;
 }
 
 int zero_len_app_zero(void) {
     int v[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int *a = arr_Init(0, 0);
-    a = arr_App(a, 0, 0);
-    if(arr_Len(a) != 0) return 1;
-    arr_Free(a);
-    a = arr_Init(sizeof(int), 0);
-    a = arr_App(a, v, 0);
-    if(arr_Len(a) != 0) return 1;
-    arr_Free(a);
+    int *a = arr_Create(0, 0);
+    a = arr_Append(a, 0, 0);
+    if(arr_Length(a) != 0) return 1;
+    arr_Destroy(a);
+    a = arr_Create(0, sizeof(int));
+    a = arr_Append(a, v, 0);
+    if(arr_Length(a) != 0) return 1;
+    arr_Destroy(a);
     if(v[0] != 9) return 1;
     if(v[1] != 8) return 1;
     if(v[2] != 7) return 1;
@@ -80,20 +80,20 @@ int zero_len_app_zero(void) {
 
 int non_zero_len_app_zero(void) {
     int v[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int *a = arr_Init(0, 9);
-    a = arr_App(a, 0, 0);
-    if(arr_Len(a) != 9) return 1;
-    arr_Free(a);
-    a = arr_Init(sizeof(int), 3);
+    int *a = arr_Create(9, 0);
+    a = arr_Append(a, 0, 0);
+    if(arr_Length(a) != 9) return 1;
+    arr_Destroy(a);
+    a = arr_Create(3, sizeof(int));
     a[0] = 2;
     a[1] = 3;
     a[2] = 5;
-    a = arr_App(a, v, 0);
-    if(arr_Len(a) != 3) return 1;
+    a = arr_Append(a, v, 0);
+    if(arr_Length(a) != 3) return 1;
     if(a[0] != 2) return 1;
     if(a[1] != 3) return 1;
     if(a[2] != 5) return 1;
-    arr_Free(a);
+    arr_Destroy(a);
     if(v[0] != 9) return 1;
     if(v[1] != 8) return 1;
     if(v[2] != 7) return 1;
@@ -108,18 +108,18 @@ int non_zero_len_app_zero(void) {
 
 int zero_len_app_non_zero(void) {
     int v[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int *a = arr_Init(0, 0);
-    a = arr_App(a, 0, 9);
-    if(arr_Len(a) != 9) return 1;
-    arr_Free(a);
-    a = arr_Init(sizeof(int), 0);
-    a = arr_App(a, v + 5, 4);
-    if(arr_Len(a) != 4) return 1;
+    int *a = arr_Create(0, 0);
+    a = arr_Append(a, 0, 9);
+    if(arr_Length(a) != 9) return 1;
+    arr_Destroy(a);
+    a = arr_Create(0, sizeof(int));
+    a = arr_Append(a, v + 5, 4);
+    if(arr_Length(a) != 4) return 1;
     if(a[0] != 4) return 1;
     if(a[1] != 3) return 1;
     if(a[2] != 2) return 1;
     if(a[3] != 1) return 1;
-    arr_Free(a);
+    arr_Destroy(a);
     if(v[0] != 9) return 1;
     if(v[1] != 8) return 1;
     if(v[2] != 7) return 1;
@@ -134,16 +134,16 @@ int zero_len_app_non_zero(void) {
 
 int non_zero_len_app_non_zero(void) {
     int v[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int *a = arr_Init(0, 9);
-    a = arr_App(a, 0, 7);
-    if(arr_Len(a) != 16) return 1;
-    arr_Free(a);
-    a = arr_Init(sizeof(int), 3);
+    int *a = arr_Create(9, 0);
+    a = arr_Append(a, 0, 7);
+    if(arr_Length(a) != 16) return 1;
+    arr_Destroy(a);
+    a = arr_Create(3, sizeof(int));
     a[0] = 2;
     a[1] = 3;
     a[2] = 5;
-    a = arr_App(a, v + 5, 4);
-    if(arr_Len(a) != 7) return 1;
+    a = arr_Append(a, v + 5, 4);
+    if(arr_Length(a) != 7) return 1;
     if(a[0] != 2) return 1;
     if(a[1] != 3) return 1;
     if(a[2] != 5) return 1;
@@ -151,7 +151,7 @@ int non_zero_len_app_non_zero(void) {
     if(a[4] != 3) return 1;
     if(a[5] != 2) return 1;
     if(a[6] != 1) return 1;
-    arr_Free(a);
+    arr_Destroy(a);
     if(v[0] != 9) return 1;
     if(v[1] != 8) return 1;
     if(v[2] != 7) return 1;
