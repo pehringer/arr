32d4304ac8c4cbb8d5f93e018d8e46ea3d6463df
pehringer <pehringer.jacob@gmail.com>
2024-09-08 14:24:01 -0600

    better example code

--- examples/factorial.c
+++ examples/factorial.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include "arr.h"
 
-int main(void) {
-  int factorial = 12;
+int factorial(int number) {
   int *n = arr_Create(0, sizeof(int));
-
-  for(int i = 2; i <= factorial; i++) {
+  for(int i = 2; i <= number; i++) {
     n = arr_Append(n, &i, 1);
   }
-  factorial = 1;
+  number = 1;
   for(int i = 0; i < arr_Length(n); i++) {
-    factorial *= n[i];
+    number *= n[i];
   }
-
   arr_Destroy(n);
-  printf("%d\n", factorial);
-  return 0;
+  return number;
 }
 
+int main(void) {
+  printf("1! = %d\n", factorial(1));
+  printf("2! = %d\n", factorial(2));
+  printf("3! = %d\n", factorial(3));
+  printf("4! = %d\n", factorial(4));
+  printf("5! = %d\n", factorial(5));
+  printf("6! = %d\n", factorial(6));
+  return 0;
+}
