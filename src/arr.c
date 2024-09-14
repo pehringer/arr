35b6b307e4da702786055b90baf5384eb5d019ba
pehringer <pehringer.jacob@gmail.com>
2024-09-14 13:00:29 -0600

    unsigned pointer

--- src/arr.c
+++ src/arr.c
@@ -17,29 +17,29 @@ void* arr_Create(size_t length, size_t size) {
     }
     a->length = length;
     a->size = size;
-    char *d = (char*) a + PADDED;
+    unsigned char *d = (unsigned char*) a + PADDED;
     memset(d, 0, length * size);
     return d;
 }
 
 void arr_Destroy(void *array) {
-    arr_t *a = (arr_t*) ((char*) array - PADDED);
+    arr_t *a = (arr_t*) ((unsigned char*) array - PADDED);
     free(a);
 }
 
 size_t arr_Length(const void *array) {
-    arr_t *a = (arr_t*) ((char*) array - PADDED);
+    arr_t *a = (arr_t*) ((unsigned char*) array - PADDED);
     return a->length;
 }
 
 void* arr_Append(void *array, const void *source, size_t length) {
-    arr_t *a = (arr_t*) ((char*) array - PADDED);
+    arr_t *a = (arr_t*) ((unsigned char*) array - PADDED);
     a->length += length;
     a = realloc(a, PADDED + a->length * a->size);
     if(a == 0) {
         return 0;
     }
-    char *d = (char*) a + PADDED;
+    unsigned char *d = (unsigned char*) a + PADDED;
     memcpy(d + (a->length - length) * a->size, source, length * a->size);
     return d;
 }
